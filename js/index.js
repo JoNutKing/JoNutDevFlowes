89c488294d6769ff9273205770dda80e4905cb64
Wellfit-Bangkok-Rajanakarn-building <boonwarutnut02@gmail.com>
2026-03-11 16:37:46 +0700

    Update index.js const text

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = `Please open and Look..`.split('')
+const text = `Please open`.split('')
 
 // Create container for better responsive layout
 title.style.display = 'flex'
